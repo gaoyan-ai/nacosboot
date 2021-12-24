2f6cc534726b7148fc2c394b260e96db759842ef
gaoyan-ai <2049330335@qq.com>
2021-12-24 17:24:25 +0800

    配置服务发现

---
+++ nacos-spring-boot-samples/nacos-discovery-sample/src/main/java/com/alibaba/boot/nacos/sample/DiscoveryController.java
@@ -0,0 +1,32 @@
+package com.alibaba.boot.nacos.sample;
+
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.pojo.Instance;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * @author： gy.sz
+ * @date： 2021/12/24
+ * @description：
+ * @modifiedBy：
+ */
+@Controller
+@RequestMapping("discovery")
+public class DiscoveryController {
+
+    @NacosInjected
+    private NamingService namingService;
+
+    @RequestMapping(value = "/get")
+    @ResponseBody
+    public List<Instance> get(@RequestParam String serviceName) throws NacosException {
+        return namingService.getAllInstances(serviceName);
+    }
+}
